52389f7863c6569f039445f4a9501ed7542da51f
unknown <jeffreyhshapiro@gmail.com>
2016-02-08 22:21:12 -0500

    refactored code to interact with express module, data is retrieved from form

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var PORT = process.env.PORT || 8090;
+
+app.use(express.static("public"));
+app.use(express.static("static"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/home.html");
+});
+
+app.post('/inquiry', function(req, res){
+  console.log(req.body.name);
+  console.log(req.body.email);
+  console.log(req.body.message);
+});
+
+app.listen(PORT, function(req, res){
+  console.log('server is listening on port %s', PORT)
+})
